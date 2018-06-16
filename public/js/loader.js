6ad12815beaa188705e06f48b6a1162f213546a9
Louis Lubineau <spoownz@yahoo.com>
2018-06-16 21:29:54 +0200

    Added some things

--- public/js/loader.js
+++ public/js/loader.js
@@ -38,3 +38,17 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn();
+        }else{
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
