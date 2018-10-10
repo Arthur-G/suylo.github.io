3824dd5eb820c1e8116be8fbb492e029dc807e7a
Suylo <spoownz@yahoo.com>
2018-10-10 20:29:05 +0200

    Added some things

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,69 +1,69 @@
 $(document).ready(function() {
 
     setTimeout(function(){
-        $('body').addClass('aa_loaded');
+        $('body').addClass('loaded');
     }, 1750);
 
     window.sr = ScrollReveal({ reset: false });
 
 
-    sr.reveal('.aa_foo', {
+    sr.reveal('.foo', {
         duration: 1500,
     });
 
-    sr.reveal('.aa_foo-top', {
+    sr.reveal('.foo-top', {
         origin: 'top',
         distance: '2em',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-left', {
+    sr.reveal('.foo-left', {
         origin: 'right',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-right', {
+    sr.reveal('.foo-right', {
         origin: 'right',
         distance: '25px',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-bottom', {
+    sr.reveal('.foo-bottom', {
         origin: 'bottom',
         distance: '50em',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-bottom2', {
+    sr.reveal('.foo-bottom2', {
         origin: 'bottom',
         distance: '30em',
         duration: 1000
     });
 
-    sr.reveal('.aa_foo-bottom3', {
+    sr.reveal('.foo-bottom3', {
         origin: 'bottom',
         distance: '30em',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-bottom4', {
+    sr.reveal('.foo-bottom4', {
         origin: 'bottom',
         distance: '30em',
         duration: 2000
     });
-    sr.reveal('.aa_foo-top2', {
+    sr.reveal('.foo-top2', {
         origin: 'top',
         distance: '5em',
         duration: 1000
     });
 
-    sr.reveal('.aa_foo-top3', {
+    sr.reveal('.foo-top3', {
         origin: 'top',
         distance: '5em',
         duration: 1500
     });
 
-    sr.reveal('.aa_foo-top4', {
+    sr.reveal('.foo-top4', {
         origin: 'top',
         distance: '5em',
         duration: 2000
